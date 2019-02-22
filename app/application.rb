4f8e75ef70d5ec70464281fed4a8c412103aa391
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-22 17:16:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    end
+    
     else
       resp.write "Path Not Found"
     end
