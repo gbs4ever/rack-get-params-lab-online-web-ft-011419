d6ad70f1c2202c69fd43c55c57a507b80b2b2e7d
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-22 17:10:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
-      end
+    end
     elsif req.path.match(/cart/)
         @@cart.each do |item|
           resp.write "#{item}\n"
@@ -17,6 +17,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    end
     else
       resp.write "Path Not Found"
     end
