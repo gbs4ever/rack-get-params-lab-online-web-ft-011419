37ee081e9890ac02100844a5af89eea95c98bd2b
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-22 18:50:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -20,7 +20,7 @@ class Application
     end
   elsif req.path.match(/add/)
     search_term = req.params["q"]
-
+    resp.write   add_search(search_term)
   end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
@@ -45,7 +45,7 @@ class Application
   end
   def add_search(search_term)
     if @@items.include?(search_term)
-    @@cart <<  "#{search_term} "
+      @@cart <<  "#{search_term} "
     else
       return "Couldn't find #{search_term}"
     end
