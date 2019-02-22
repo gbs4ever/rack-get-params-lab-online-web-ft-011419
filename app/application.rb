13f80f4ff44b0d29e192d629d8be7e6e1a466fa8
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-22 18:49:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,10 @@ class Application
           resp.write "#{item}\n"
       end
     end
+  elsif req.path.match(/add/)
+    search_term = req.params["q"]
+
+  end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
@@ -35,6 +39,13 @@ class Application
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+  def add_search(search_term)
+    if @@items.include?(search_term)
+    @@cart <<  "#{search_term} "
     else
       return "Couldn't find #{search_term}"
     end
